91c6f39cb737130133f4c1ea828c9087b2e860fc
Dmidd87 <dmidd87@gmail.com>
2015-03-11 14:30:57 -0600

    Can now insert into mongo with js file alteration with tasks

--- simple-todos.js
+++ simple-todos.js
@@ -1,10 +1,10 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   Template.body.helpers({
-    tasks: [
-      { text: "This is task 1" },
-      { text: "This is task 2" },
-      { text: "This is task 3" }
-    ]
+    tasks: function () {
+      return Tasks.find({});
+    }
   });
 
   Template.hello.events({
@@ -19,10 +19,4 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
-}
-
-
-if (Meteor.isClient) {
-  // This code only runs on the client
-
 }
