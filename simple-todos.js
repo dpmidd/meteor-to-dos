9afb0a6c108809c514629c6b2d93b75999097472
Dmidd87 <dmidd87@gmail.com>
2015-03-11 14:17:41 -0600

    First commit

---
+++ simple-todos.js
@@ -0,0 +1,28 @@
+if (Meteor.isClient) {
+  Template.body.helpers({
+    tasks: [
+      { text: "This is task 1" },
+      { text: "This is task 2" },
+      { text: "This is task 3" }
+    ]
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
+
+
+if (Meteor.isClient) {
+  // This code only runs on the client
+
+}
